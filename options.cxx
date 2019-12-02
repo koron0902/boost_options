ded38e7867cdc627e73154583cc0b66c56645337
Keita <koron0902@gmail.com>
2019-12-02 14:14:16 +0900

    first commit

---
+++ options.cxx
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <boost/program_options.hpp>
+
+int main(const int argc, const char* argv[]){
+	using boost::program_options::options_description;
+	using boost::program_options::parse_command_line;
+	using boost::program_options::value;
+	using boost::program_options::variables_map;
+	using boost::program_options::store;
+
+	options_description desc("おぷしょん");
+	desc.add_options()
+			("nyan, n", value<int>()->default_value(3), "にゃ～ん")
+			("myon, m", value<std::string>()->default_value("みょみょみょ"), "みょん")
+			("help, h", "へるぷ");
+
+	variables_map vm;
+	store(parse_command_line(argc, argv, desc), vm);
+	auto nyan = vm["nyan"].as<int>();
+	auto myon = vm["myon"].as<std::string>();
+
+	std::cout << "nyan: " << nyan << std::endl;
+	std::cout << "myon: " << myon << std::endl;
+
+
+}
